85191763edb95bb654612931bf9813beeebe1019
LeonZhu <zl937154020@163.com>
2019-02-12 16:46:24 +0800

    图片删除

--- src/main/java/com/hyt/service/HytImgsService.java
+++ src/main/java/com/hyt/service/HytImgsService.java
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Service;
 public class HytImgsService {
     @Autowired
     private HytImgsRepository hytImgsRepository;
+
+    public deleteImg
 }
--- src/main/java/com/hyt/web/HytImgsController.java
+++ src/main/java/com/hyt/web/HytImgsController.java
@@ -2,7 +2,11 @@ package com.hyt.web;
 
 import com.hyt.service.HytImgsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class HytImgsController {
     @Autowired
     private HytImgsService hytImgsService;
+
+    @PostMapping("/deleteImgs")
+    public ResponseEntity login(@RequestParam String houseId, @RequestParam String id) {
+        try {
+
+        }catch (Exception e){
+            e.printStackTrace();
+            ExecResult er = new ExecResult(false,e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(er);
+        }
+    }
 }
